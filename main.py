c88b8144fd41ea5624fdbbf6cf6a2d5173fbaecc
PDUTeacher <tmv.developer@gmail.com>
2024-06-20 23:05:30 +0300

    templstes

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>My site</p>"
+    return render_template('index.html')
 
 @app.route("/test")
 def test():
     return "<p>Test</p>"
 
-# app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
